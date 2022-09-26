68dd0fc7de0ede02c8dbf89f894dca3f770f64ce
IlyaChaban <ilyachaban@gmail.com>
2022-09-26 20:01:36 +0200

    Correcting algorithm according to the task

--- 1DU_Buzz_Fizz/Fizz_Buzz.py
+++ 1DU_Buzz_Fizz/Fizz_Buzz.py
@@ -1,17 +1,16 @@
-import pytest
-def Fizz_Buzz():
+def Fizz_Buzz(number_of_rounds):
     output_string=''
-    for i in range(1,37):
+    for i in range(1,number_of_rounds):
         if i%3==0 and i%5==0:
             output_string+='Fizz Buzz, '
+            print('Fizz Buzz')
         elif i%3==0:
             output_string+='Fizz, '
+            print('Fizz')
         elif i%5==0:
             output_string+='Buzz, '
+            print('Buzz')
         else:
             output_string+=str(i)+', '
-    return(output_string+'...')
-    
-def test_Fizz_Buzz():
-    assert Fizz_Buzz()== '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz, ...'
-pytest.main()
+            print(str(i))
+    print('Printing finished')
--- 1DU_Buzz_Fizz/test_Fizz_Buzz .py
+++ 1DU_Buzz_Fizz/test_Fizz_Buzz .py
@@ -2,4 +2,4 @@ import pytest
 from Fizz_Buzz import Fizz_Buzz
     
 def test_Fizz_Buzz():
-    assert Fizz_Buzz()== '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz, ...'
+    assert Fizz_Buzz(37)== '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz, ...'
