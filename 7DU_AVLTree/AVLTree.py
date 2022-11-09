7c6b6f455ecbf785e6ffc6dd249c0829dd0b8429
IlyaChaban <ilyachaban@gmail.com>
2022-11-09 21:34:13 +0100

    very tuff but can't find mistake

--- 7DU_AVLTree/AVLTree.py
+++ 7DU_AVLTree/AVLTree.py
@@ -3,55 +3,80 @@ class Node:
         self.data = data
         self.left = None
         self.right = None
+        self.equlibrium = None
 
-    def insert(self, tree, data):
+    def insert(self, data):
         if self.data is not None:
-            if data<self.data:
+            if data < self.data:
                 if self.left is None:
-                    self.left=Node(data)
+                    self.left = Node(data)
                 else:
                     self.left.insert(data)
-            elif data>self.data:
+            elif data > self.data:
                 if self.right is None:
-                    self.right=Node(data)
+                    self.right = Node(data)
                 else:
                     self.right.insert(data)
         else:
-            self.data=data
-            
-        self.level_tree()
-    def level_tree(self,tree):
-        if self.balance<=-2:
-            Node.left_rotation(tree)
-        if self.balance >= 2:
-            Node.right_rotation(tree)
+            self.data = data
+        
+        self.equlibrium_calc()
+        self.balance_tree(self)
 
+    def balance_tree(self,tree):
+        self.equlibrium_calc()
+        
+        if self.equlibrium > 1 and self.data < self.left.data:
+            Node.rotate_right(tree)
+            tree.equlibrium_calc()
+        if self.equlibrium < -1 and self.data > self.right.data:
+            Node.rotate_left(tree)
+            tree.equlibrium_calc()
+        # if self.equlibrium > 1 and self.data > self.left.data:
+            # Node.rotate_left(tree)
+            # Node.rotate_right(tree)
+            # tree.equlibrium_calc()
+        # if self.equlibrium < -1 and self.data < self.right.data:
+            # Node.rotate_right(tree)
+            # Node.rotate_left(tree)
+            # tree.equlibrium_calc()
             
         if tree.right is not None: 
-            self.balance(tree.right)
+            self.balance_tree(tree.right)
         if tree.left is not None:
-            self.balance(tree.left)
-    
-    def right_rotation(self, z):
-        y = z.left
-        T3 = y.right
+            self.balance_tree(tree.left)
 
-        y.right = z
-        z.left = T3
-        return y
+    def equlibrium_calc(self) -> int:
+        if self.data is not None:
+            self.equlibrium = self.max_depth(self.left) - self.max_depth(self.right)
+            if self.left is not None:
+                self.left.equlibrium_calc()
+            if self.right is not None:
+                self.right.equlibrium_calc()
 
-    def left_rotation(self, z):
-        y = z.right
-        T2 = y.left
+    def rotate_right(self):
+        right_node=Node(self.data)
+        
+        if self.right is not None:
+            right_node.right=self.right
+        if self.left is not None:
+            if self.left.right is not None:
+                right_node.left=self.left.right
+            self.data=self.left.data
+            self.left=self.left.left
+        self.right=right_node
 
-        y.left = z
-        z.right = T2
-        return y
-
-    def balance(self) => bool, int:
-        left_depth = self.max_depth(self.left)
-        right_depth = self.max_depth(self.right)
-        return left_depth - right_depth
+    def rotate_left(self):
+        left_node=Node(self.data)
+        
+        if self.left is not None:
+            left_node.left=self.left
+        if self.right is not None:
+            if self.right.left is not None:
+                left_node.right=self.right.left
+            self.data=self.right.data  
+            self.right=self.right.right
+        self.left=left_node
 
     def  print_tree(self,tree, level = 0, prefix = ""):
         if tree.data:
@@ -60,7 +85,6 @@ class Node:
                 self.print_tree(tree.left, level = level +1, prefix = "L:")
             if tree.right:
                 self.print_tree(tree.right, level = level +1, prefix = "R:")
-
 
     def in_order_traversal(self, tree):
         result = []
@@ -73,7 +97,6 @@ class Node:
     def max_depth(self, tree, depth=0):
         if tree is None:
             return depth-1
-        
         return max(self.max_depth(tree.left, depth=depth +1),
                    self.max_depth(tree.right, depth=depth +1))
 
@@ -97,11 +120,12 @@ tree.insert(5)
 tree.insert(6)
 tree.insert(7)
 tree.print_tree(tree)
-print(tree.in_balance(tree))
-#assert tree.in_order_traversal(tree) == [1, 2, 3, 4, 5, 6, 7]
-#assert tree.contains(tree, 3)
-#assert not tree.contains(tree, 20)
-#assert tree.max_depth(tree) <= 2
+
+print(tree.in_order_traversal(tree))
+assert tree.in_order_traversal(tree) == [1, 2, 3, 4, 5, 6, 7]
+assert tree.contains(tree, 3)
+assert not tree.contains(tree, 20)
+assert tree.max_depth(tree) <= 2
 
 tree = Node()
 
@@ -111,4 +135,4 @@ tree.insert(3)
 tree.insert(5)
 tree.insert(100)
 
-#assert tree.max_depth(tree) <= 2
+assert tree.max_depth(tree) <= 2
